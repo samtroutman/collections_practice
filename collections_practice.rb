0e44fe2718aa7eb70713810ad314227fd6b8eb08
Sam <sam@Sams-MacBook-Air.local>
2020-11-22 17:48:50 -0500

    commit tree effe607405ad5c7d4bd17a1f865c7f2c31b1e109

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,46 @@
+def sort_array_asc(array)
+    array.sort 
+end
+
+def sort_array_desc(array)
+    array.sort do |a, b|
+        b <=> a
+    end
+end
+
+def sort_array_char_count(array)
+    array.sort {|left, right| left.length <=> right.length}
+end
+
+def swap_elements(array)
+    array[1],array[2]=array[2],array[1]
+    return array  
+end
+
+def reverse_array(array)
+    array.reverse 
+end
+
+def kesha_maker(array)
+    array.map { |word| word[2] = "$" }
+    array
+end
+
+def find_a(array)
+    array.select{|n| n[0] =="a"}
+end
+
+def sum_array(array)
+    array.inject { |add, num| add+num }
+end
+
+def add_s(array)
+    array.each_with_index.collect do |element, index|  
+    if index == 1
+      element = element
+    else
+      element = element + "s"
+  end
+end
+end
 
